2edfff0900e0849d6c9e29af9c9b4d97fe5447ed
Ulmanis <linards.kupcis@gmail.com>
2020-05-20 18:19:01 +0300

    ver 18:18

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,44 @@ namespace TicTacToe
 {
     public partial class Form1 : Form
     {
+        bool xPlayerTurn = true;
+
         public Form1()
         {
             InitializeComponent();
+            InitializeGrid();
+            InitializeCells();
+        }
+
+        private void InitializeGrid()
+        {
+            Grid.BackColor = Color.LightCoral;
+            Grid.CellBorderStyle = TableLayoutPanelCellBorderStyle.Inset;
+        }
+
+        private void InitializeCells()
+        {
+            string labelName;
+            for(int i = 1; i <= 9; i++)
+            {
+                labelName = "label" + i;
+                Grid.Controls[labelName].Text = string.Empty;
+            }
+        }
+
+        private void Player_Click(object sender, EventArgs e)
+        {
+            Label label = (Label)sender;
+            if (xPlayerTurn)
+            {
+                label.Text = "X";
+            }
+            else
+            {
+                label.Text = "O";
+            }
+            xPlayerTurn = !xPlayerTurn;
+            
         }
     }
 }
